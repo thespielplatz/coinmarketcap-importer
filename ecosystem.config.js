e71153825f386c07080fbf5669dc706f8f19a6d9
Philipp Horwath <fil@v2202201165610176076.goodsrv.de>
2022-03-01 15:00:13 +0100

    added eco file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [
+  {
+      name   : "Coinmarketcap Importer",
+      script : "./app.js",
+	  env    : {
+                  NODE_PORT: 2001
+          }
+    },
+  ]
+}
+
